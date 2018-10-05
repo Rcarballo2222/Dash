3b19e1ea990133cbbb87769585007a6e17e0812a
Rene Carballo <rcarballo2222@gmail.com>
2018-10-05 00:34:24 -0500

    Added wifi connect LED

---
+++ Etc/wifi.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import os
+import time
+import urllib2 
+
+os.system("gpio mode 6 out && gpio mode 5 out")
+
+while True:
+    try:
+        urllib2.urlopen("http://www.google.com").close()
+    except urllib2.URLError:
+        print "Not Connected"
+        os.system("gpio write 21 0 && gpio write 22 1")
+        time.sleep(1)
+    else:
+        print "Connected"
+        os.system("gpio write 21 1 && gpio write 22 0")
+        break
