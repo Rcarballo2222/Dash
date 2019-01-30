38f7cd2f16bab2279ab593a2299422ce4e6f8e4a
Rcarballo2222 <rcarballo2222@gmail.com>
2019-01-30 00:03:44 +0000

    Small changes

--- APIs/RA8875 LCD Controller/test.cpp
+++ APIs/RA8875 LCD Controller/test.cpp
@@ -9,7 +9,7 @@ RA8875 d = RA8875(0, 13);
 int main(int argc, char const *argv[])
 {
     d.begin(RA8875_800x480 ,16);
-    d.clearMemory(false);
-    d.displayOn(true);
+    //d.clearMemory(false);
+    //d.displayOn(true);
     return 0;
 }
